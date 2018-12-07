90f190fde2db10b1e44f2f0a5fc640f7026acd7d
SpInAgRblzzz <36811343+SpInAgRblzzz@users.noreply.github.com>
2018-12-07 20:08:58 +0300

    Начата функция метода "голых пар"
    написан метод для ряда

--- src/index.js
+++ src/index.js
@@ -106,9 +106,97 @@ module.exports = function solveSudoku(matrix) {
     }
   }
 
+  function findOpenPair(){
+    for(let row = 0; row < 9; row++){
+      for(let column = 0; column < 9; column++){
+        if(result[row][column].length === 2){
+          let first = result[row][column][0];
+          let second = result[row][column][1];
+          function arrayCheck(item){
+            return item === first || item === second
+          }
+
+
+          //проверка ряда
+          let rowCounter = 1;
+          let rowCoordinate = 0;
+          for(let secCol = 0; secCol < 9; secCol++){
+            if(result[row][secCol].length === 2 && result[row][secCol].every(arrayCheck)&&secCol!=column){
+              rowCounter++;              
+              if(rowCounter > 2){break}
+              rowCoordinate = secCol;
+            }
+            if(secCol === 8 && rowCounter === 2){
+              for(let i = 0; i < 9; i++){
+                if(i != column && i != rowCoordinate && typeof(result[row][i]) === 'object'){
+                  let j = result[row][i].indexOf(first);
+                  if(j!=-1){result[row][i].splice(j,1)}
+
+                  let k = result[row][i].indexOf(second);                  
+                  if(k!=-1){result[row][i].splice(k,1)}
+                }
+              }
+            }
+          }
+          /*
+          //проверка колонки
+          for(let secRow = 0; secRow < 9; secRow++){
+            if(typeof(result[secRow][column]) === 'object' && secRow != row){
+              if(result[secRow][column].indexOf(candidate) != -1){break}
+            }
+            if(secRow === 8){
+              result[row][column] = candidate;
+              return fillEmpty()
+            }
+          }
+
+          //проверка ячейки
+          let startRow = 0;
+          if(row >= 0 && row <= 2){startRow = 0}
+          if(row >= 3 && row <= 5){startRow = 3}
+          if(row >= 6 && row <= 8){startRow = 6}
+
+          let startColumn = 0;
+          if(column >= 0 && column <= 2){startColumn = 0}
+          if(column >= 3 && column <= 5){startColumn = 3}
+          if(column >= 6 && column <= 8){startColumn = 6}
+
+          for(let i = startRow;i <= startRow + 2; i++){
+            for(let j =startColumn; j <= startColumn + 2; j++){
+              if(typeof(result[i][j]) === 'object' && !(i === row && j === column)){
+                if(result[i][j].indexOf(candidate) != -1){return result}                  
+              }
+              if(i === startRow +2 && j === startColumn + 2){
+                result[row][column] = candidate;
+                return fillEmpty()
+              }
+            }
+          }*/
+        }
+      }
+    } 
+  }
 
   fillEmpty()
   checkCandidates()
+  findOpenPair()
 
   return result
 }
+
+/*
+///тест на голые пары
+
+solveSudoku( [
+  [4,0,0,0,0,0,9,3,8],
+  [0,3,2,0,9,4,1,0,0],
+  [0,9,5,3,0,0,2,4,0],
+  [3,7,0,6,0,9,0,0,4],
+  [5,2,9,0,0,1,6,7,3],
+  [6,0,4,7,0,3,0,9,0],
+  [9,5,7,0,0,8,3,0,0],
+  [0,0,3,9,0,0,4,0,0],
+  [2,4,0,0,3,0,7,0,9]
+  ])
+
+  */
